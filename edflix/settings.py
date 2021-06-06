6e6f45989c250e49ff339abae8d0179c1b958acb
Yash-K-B <yashwantkumar027@gmail.com>
2021-06-06 13:27:33 +0530

    Update settings.py

--- edflix/settings.py
+++ edflix/settings.py
@@ -146,14 +146,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-    '/var/www/static/',
-]
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 
 LOGIN_REDIRECT_URL="/"
